6234088173862fa82ba2ce589e8627971e06f9e8
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-06-26 00:37:09 -0400

    started on the app.js code. added constants and api key

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '1f6368cc8815d1f8d0682ecff32d673a';
+    const searchForm = document.getElementById('searchForm');
+    const cityInput = document.getElementById('cityInput');
+    const currentWeatherDetails = document.getElementById('currentWeatherDetails');
+    const forecastDetails = document.getElementById('frecastDetails');
+    const historyList = document.getElementById('historyList');
+});
